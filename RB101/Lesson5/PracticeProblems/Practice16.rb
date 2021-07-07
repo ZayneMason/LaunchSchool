06640428a66419c9321a7a357e2d9891d857297b
ZayneMason <zayne.c.mason@gmail.com>
2021-07-06 21:55:08 -0700

    fixed some stuff

--- RB101/Lesson5/PracticeProblems/Practice16.rb
+++ RB101/Lesson5/PracticeProblems/Practice16.rb
@@ -9,7 +9,7 @@ def uuid_generator
   # Going to use a sections array to set how many times to randomly add characters to uuid string
   sections = [8, 4, 4, 4, 12]
   # Settins an array to sample characters from
-  letters_and_nums = ('a'..'z').to_a + ('0'..'9').to_a
+  letters_and_nums = ('a'..'f').to_a + ('0'..'9').to_a
   sections.each do |values|
     values.times {uuid << letters_and_nums.sample }
     uuid << '-'
