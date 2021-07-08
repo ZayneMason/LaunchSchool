6e3b7b71128d651daa745a2ed9cd344295455f5a
ZayneMason <zayne.c.mason@gmail.com>
2021-07-07 21:13:49 -0700

    added tic tac toe game

---
+++ RB101/Lesson6/tic_tac_toe.rb
@@ -0,0 +1,100 @@
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts " #{brd[1]}   |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[4]}   |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts " #{brd[7]}   |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+end
+
+def startboard
+  board_slots = {}
+  (1..9).each { |slot| board_slots[slot] = slot }
+  board_slots
+end
+
+def winner?(brd)
+  if brd[1] == brd[2] && brd[1] == brd[3]
+    true
+  elsif brd[4] == brd[5] && brd[4] == brd[6]
+    true
+  elsif brd[7] == brd[8] && brd[7] == brd[9]
+    true
+  elsif brd[1] == brd[5] && brd[1] == brd[9]
+    true
+  elsif brd[3] == brd[5] && brd[3] == brd[7]
+    true
+  elsif brd[1] == brd[4] && brd[1] == brd[7]
+    true
+  elsif brd[3] == brd[6] && brd[3] == brd[9]
+    true
+  elsif brd[2] == brd[5] && brd[2] == brd[8]
+  else
+    false
+  end
+end
+
+
+
+loop do
+
+  board = startboard
+  display_board(board)
+
+  loop do
+
+    loop do
+      puts '=> Please input your choice. (number marking space)'
+      user_choice = gets.chomp.to_i
+      if board[user_choice] == 'X' || board[user_choice] == 'O'
+        puts '=> Please enter a space that is not taken.'
+        next
+      elsif (1..9).include?(user_choice) == false
+        next
+      else
+        board[user_choice] = 'X'
+      end
+      display_board(board)
+      break
+    end
+
+    if winner?(board)
+      puts '=> You win!'
+      break
+    end
+
+    puts "=> Computer's move."
+    loop do
+      cpu_choice = (1..9).to_a.sample
+      if board[cpu_choice] == 'X' || board[cpu_choice] == 'O' 
+        next
+      else
+        board[cpu_choice] = 'O'
+      end
+      display_board(board)
+      break
+    end
+
+    if winner?(board)
+      puts '=> Computer wins.'
+      break
+    end
+
+  end
+
+  puts '=> Would you like to play again? (y/n)'
+  decision = gets.chomp.downcase
+  if decision == 'n'
+    puts '=> Goodbye.'
+    break
+  else
+    next
+  end
+
+end
