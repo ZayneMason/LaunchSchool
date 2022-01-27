14a6ce3568e6acf821b5496c0fcab03876866884
ZayneMason <zayne.c.mason@gmail.com>
2022-01-26 19:36:43 -0800

    finished reduce_method

--- RB130/Lesson1/reduce_method.rb
+++ RB130/Lesson1/reduce_method.rb
@@ -2,3 +2,18 @@
 # or "accumulate" a collection into 1 object. What the specific folding or accumulation
 # mechanism is depends on the code passed in to the block.
 
+# Unlike #each, reduce yields two arguments to the block, the first being the 'accumulator' object,
+# and the second being the second being the 'current' object.
+
+def reduce(arr)
+  counter = 0
+  accumulator = 0
+  while counter < arr.size
+    accumulator = yield(accumulator, arr[counter])
+    counter += 1
+  end
+
+  accumulator
+end
+
+
